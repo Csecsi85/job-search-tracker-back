ccb9b3cd1808316aa48ef4210398b4edbafe77ac
Gabor Csecsetka <csecsi85@gmail.com>
2021-06-19 22:46:13 +0100

    Installed base modules and configured eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ["prettier"],
+  rules: {
+    "arrow-body-style": "warn",
+    "no-unused-vars": "warn",
+    "comma-dangle": ["error", "always-multiline"],
+    "default-case": "error",
+    "prettier/prettier": ["error"],
+  },
+};
